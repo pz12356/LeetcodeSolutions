9113ec1d99e4c6485140f2f44e9aad2460e63f69
pppzz <387621994@qq.com>
2024-09-18 18:32:25 +0800

    Add MergeTwoSortedLists problem

---
+++ src/EasyDifficulty/MergeTwoSortedLists/ListNode.java
@@ -0,0 +1,9 @@
+package EasyDifficulty.MergeTwoSortedLists;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
