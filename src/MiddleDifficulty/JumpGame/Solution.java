71189c1c25ab0787d79d176ca7747132bcdc61f8
pppzz <387621994@qq.com>
2024-09-24 16:20:40 +0800

    Add JumpGame problem

---
+++ src/MiddleDifficulty/JumpGame/Solution.java
@@ -0,0 +1,25 @@
+package MiddleDifficulty.JumpGame;
+
+import java.util.Scanner;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, nums[i] + i);
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println(new Solution().canJump(nums));
+    }
+}
